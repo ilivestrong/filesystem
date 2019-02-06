185f620e85c50a8f9640c72ec31cc794a9215994
deepak <deepak@central.tech>
2019-02-06 12:44:36 +0700

    Add a basic example using fs.open() and fs.fstats() which utilize system programming concept of file description(a type of handle).

--- src/basic.js
+++ src/basic.js
@@ -1,6 +1,25 @@
+const fs = require('fs')
 
 const init = () => {
     // write your awesome code here...
     console.log('Welcome to fs module.')
+
+    fs.open(__filename, 'r', (err, fd) => {
+        if (err) throw err
+        else getFileStats(fd)
+    })
+}
+
+const getFileStats = (handle) => {
+    fs.fstat(handle, (err, stats) => {
+        if (err) console.log(`Encountered error: ${err}`)
+        else {
+            console.log(stats)
+            fs.close(handle, (err) => {
+                if (err) throw err
+                else console.log(`\nStatistics printed, handle:{${handle}} closed.`)
+            })
+        }
+    })
 }
 module.exports = init
